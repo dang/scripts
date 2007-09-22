d83ef36287d0fe9863f4f006ee05f7fa1fda5dff
Daniel Gryniewicz <dang@fprintf.net>
2007-09-21 23:35:44 -0400

    Add splitname to split up package names into versions/revisions

--- portage-functions.sh
+++ portage-functions.sh
@@ -55,3 +55,45 @@ function portcd() {
 function port_category() {
 	PORT_CATEGORY=$(basename $(dirname ${PWD}))
 }
+
+#
+# splitname (pvr|pn|pv|revision) package-name-version-revision
+# Borrowed from /usr/bin/ebump
+#
+splitname() {
+    case $1 in
+	pvr)
+	    echo ${2} | sed -r "s/.*-([0-9].*)/\1/"
+	    ;;
+	pn)
+	    pn=$(echo ${2} | sed -r "s/(.*)-[0-9].*/\1/")
+	    if [ ${pn} == ${2} ] ; then
+		if [ $(echo ${2} | grep "^[0-9].*") ] ; then
+		    # The filename starts with a version number, thus it has no pn
+		    pn=""
+		else
+		    # The filename doesn't have a recognizeable version number everything is a pn
+		    pn=${2}
+		fi
+	    fi
+	    echo ${pn}
+	    ;;
+	revision)
+	    rev=$(echo ${2} | sed -r "s/.*-r([0-9][0-9]*)/\1/")
+	    if [ ${rev} == ${2} ] ; then
+		rev=0
+	    fi
+	    echo ${rev}
+	    ;;
+	pv)
+	    pv=$(echo ${2} | sed -r "s/.*-([0-9].*)-r[0-9]+/\1/")
+	    if [ ${pv} == ${2} ] ; then
+		pv=$(echo ${2} | sed -r "s/.*-([0-9].*)/\1/")
+	    fi
+	    echo ${pv}
+	    ;;
+	*)
+	    echo
+    esac
+}
+
