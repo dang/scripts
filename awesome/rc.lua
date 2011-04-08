5ff148930cf0c1225ab0d660a54a149bed5259c9
Daniel Gryniewicz <dang@fprintf.net>
2011-04-08 10:00:40 -0400

    Force uzbl to be on my browser page

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -721,8 +721,13 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "pinentry" },
       properties = { floating = true } },
+    { rule = { class = "pinentry-gtk-2" },
+      properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
+      -- Force uzbl to be on my browser page
+     { rule = { class = "Uzbl-tabbed" },
+       properties = { tag = tags[1][14] } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
